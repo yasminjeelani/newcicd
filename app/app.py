df3737a7a7ebf8f51bce42c4c31b94c810dbbcab
yasminjeelani <36299748+yasminjeelani@users.noreply.github.com>
2023-06-22 19:13:42 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+sfrom flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, everyone!'
+    return 'Devops, everyone!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
