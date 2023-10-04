27f6a466ecf0ec7b186104a27ca9c0faeac9879b
yasminjeelani <36299748+yasminjeelani@users.noreply.github.com>
2023-10-04 16:10:05 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Welcome to Devops and CICD!'
+    return 'Welcome to Devops and CICD'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
